1c014d8e63b21dfa61a2778a7a4751b3d3c12d76
Julien Du <du.julien1@gmail.com>
2025-09-10 09:43:14 +0200

    Install node dependency

--- post-render.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" || -n "$COMSPEC" ]]; then
-    copy /Y "javascript\\quarto_listing.js" "_site\\site_libs\\quarto-listing\\quarto-listing.js"
-else
-    cp "javascript/quarto_listing.js" "_site/site_libs/quarto-listing/quarto-listing.js"
-fi
